2d80e795a8a1accbf6a33bf071cb84f20fe32e49
suchi <1329930823@qq.com>
2024-09-30 09:54:27 +0800

    init Project

---
+++ src/main/java/com/xama/security/XamaAdminSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.xama.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XamaAdminSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XamaAdminSecurityApplication.class, args);
+    }
+
+}
